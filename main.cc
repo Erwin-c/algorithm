a298e5fe699d36a6b203fda32ca77b848fbf9d67
Erwin <274868686@qq.com>
2021-01-28 05:21:32 +0800

    ...

--- main.cc
+++ main.cc
@@ -5,44 +5,12 @@
  *      Author: Erwin
  */
 
-#include <cassert>
-#include <iostream>
-
-#include "sort/helper.h"
-#include "sort/sort.h"
+#include "test/test.h"
 
 int main() {
-  int n = 500000;
-  // Test for random arrays.
-  std::cout << "Test for random arrays, size = " << n << ", random range [0, "
-            << n << "]" << std::endl;
-  int* arr1 = helper::GenerateRandomArray(n, 0, n);
-  int* arr2 = helper::CopyArray(arr1, n);
-  int* arr3 = helper::CopyArray(arr1, n);
-
-  helper::TestSort("Merge Sort", sort::MergeSort, arr1, n);
-  helper::TestSort("Quick Sort", sort::QuickSort, arr2, n);
-
-  delete[] arr1;
-  delete[] arr2;
-  delete[] arr3;
-
-  int swap_time = 100;
-  assert(swap_time >= 0);
-  // Test for nearly ordered arrays.
-  std::cout << "Test for nearly ordered arrays, size = " << n
-            << ", swap time = " << swap_time << std::endl;
-  arr1 = helper::GenerateNearlyOrderedArray(n, swap_time);
-  arr2 = helper::CopyArray(arr1, n);
-  arr3 = helper::CopyArray(arr1, n);
-
-  helper::TestSort("Insertion Sort", sort::InsertionSort, arr1, n);
-  helper::TestSort("Merge Sort", sort::MergeSort, arr2, n);
-  helper::TestSort("Quick Sort", sort::QuickSort, arr3, n);
-
-  delete[] arr1;
-  delete[] arr2;
-  delete[] arr3;
+  // Begin test.
+  test::sort_test_3();
+  test::sort_test_4();
 
   return 0;
 }
---
+++ test/test.h
@@ -0,0 +1,21 @@
+/*
+ * test.h
+ *
+ *  Created on: Jan 28, 2021
+ *      Author: Erwin
+ */
+
+#ifndef TEST_TEST_H_
+#define TEST_TEST_H_
+
+namespace test {
+
+// Compare merge sort and quick sort with random arrays.
+void sort_test_3();
+
+// Compare insertion sort, merge sort and quick sort with nearly ordered arrays.
+void sort_test_4();
+
+}  // namespace test
+
+#endif  // TEST_TEST_H_
