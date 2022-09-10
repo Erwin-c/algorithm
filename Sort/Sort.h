a333cabef56ba8c8f29da1783640b9eb59be436e
Erwin <274868686@qq.com>
2022-09-10 18:15:48 +0800

    ...

--- Sort/Sort.h
+++ Sort/Sort.h
@@ -311,9 +311,9 @@ void quickSort3Ways(T arr[], int n) {
 
 // Create a heap using Insert -> O(nlogn), related to 'level'
 // Sort using Exract -> O(nlogn)
-// heapSort1 -> O(nlogn)
+// heapSortInsert -> O(nlogn)
 template <typename T>
-void heapSort1(T arr[], int n) {
+void heapSortInsert(T arr[], int n) {
   MaxHeap<T> maxheap = MaxHeap<T>(n);
   for (int i = 0; i < n; ++i) {
     maxheap.insert(arr[i]);
@@ -328,9 +328,9 @@ void heapSort1(T arr[], int n) {
 
 // Optimization: Create a heap using Heapify -> O(n)
 // Sort using Exract -> O(nlogn)
-// heapSort2 -> O(nlogn) but optimized
+// heapSortHeapify -> O(nlogn) but optimized
 template <typename T>
-void heapSort2(T arr[], int n) {
+void heapSortHeapify(T arr[], int n) {
   MaxHeap<T> maxheap = MaxHeap<T>(arr, n);
 
   for (int i = n - 1; i >= 0; --i) {
