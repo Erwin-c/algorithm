4123c2a06054e032ceff8ac8a00612b7a4ad4a39
Erwin <erwin@localhost.localdomain>
2022-10-02 01:31:09 +0800

    ...

--- LeetCode/QueueSolution.h
+++ LeetCode/QueueSolution.h
@@ -7,10 +7,17 @@
 #ifndef QUEUE_SOLUTION_H_
 #define QUEUE_SOLUTION_H_
 
+#include <vector>
+
 class QueueSolution {
  public:
   // 279
   int numSquares(int n);
+
+  // 347
+  std::vector<int> topKFrequent(std::vector<int>& nums, int k);
+
+  // TBD: 347, k --> n
 };
 
 #endif  // QUEUE_SOLUTION_H_
