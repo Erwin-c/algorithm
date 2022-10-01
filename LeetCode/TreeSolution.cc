36ef7f51391ecc9ac28fc240fad22057e761a675
Erwin <erwin@localhost.localdomain>
2022-10-01 14:03:21 +0800

    ...

--- LeetCode/TreeSolution.cc
+++ LeetCode/TreeSolution.cc
@@ -36,11 +36,76 @@ std::vector<int> TreeSolution::preorderTraversal(TreeNode* root) {
       if (command.node->right != nullptr) {
         stack.push(Command("go", command.node->right));
       }
+
       if (command.node->left != nullptr) {
         stack.push(Command("go", command.node->left));
       }
 
       stack.push(Command("print", command.node));
+    }
+  }
+
+  return res;
+}
+
+std::vector<int> TreeSolution::inorderTraversal(TreeNode* root) {
+  std::vector<int> res;
+  if (root == nullptr) {
+    return res;
+  }
+
+  std::stack<Command> stack;
+  stack.push(Command("go", root));
+  while (!stack.empty()) {
+    Command command = stack.top();
+    stack.pop();
+
+    if (command.s == "print") {
+      res.push_back(command.node->val);
+    } else {
+      assert(command.s == "go");
+
+      if (command.node->right != nullptr) {
+        stack.push(Command("go", command.node->right));
+      }
+
+      stack.push(Command("print", command.node));
+
+      if (command.node->left != nullptr) {
+        stack.push(Command("go", command.node->left));
+      }
+    }
+  }
+
+  return res;
+}
+
+std::vector<int> TreeSolution::postorderTraversal(TreeNode* root) {
+  std::vector<int> res;
+  if (root == nullptr) {
+    return res;
+  }
+
+  std::stack<Command> stack;
+  stack.push(Command("go", root));
+  while (!stack.empty()) {
+    Command command = stack.top();
+    stack.pop();
+
+    if (command.s == "print") {
+      res.push_back(command.node->val);
+    } else {
+      assert(command.s == "go");
+
+      stack.push(Command("print", command.node));
+
+      if (command.node->right != nullptr) {
+        stack.push(Command("go", command.node->right));
+      }
+
+      if (command.node->left != nullptr) {
+        stack.push(Command("go", command.node->left));
+      }
     }
   }
 
