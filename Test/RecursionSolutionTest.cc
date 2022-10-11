fe528e34de953bced22eb2820f9eb3c3b8c56ef6
Erwin <erwin@localhost.localdomain>
2022-10-11 17:42:12 +0800

    ...

--- Test/RecursionSolutionTest.cc
+++ Test/RecursionSolutionTest.cc
@@ -17,4 +17,12 @@ TEST(RecursionSolutionTest, letterCombinations) {
   EXPECT_EQ(RecursionSolution().letterCombinations(digits), res);
 }
 
+TEST(RecursionSolutionTest, permute) {
+  std::vector<int> nums{1, 2, 3};
+  std::vector<std::vector<int>> res{{1, 2, 3}, {1, 3, 2}, {2, 1, 3},
+                                    {2, 3, 1}, {3, 1, 2}, {3, 2, 1}};
+
+  EXPECT_EQ(RecursionSolution().permute(nums), res);
+}
+
 }  // namespace
