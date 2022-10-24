387f4118da5f06fd24496994eadad666bdf9a289
Erwin <erwin@localhost.localdomain>
2022-10-24 12:31:06 +0800

    ...

---
+++ Test/DPSolutionTest.cc
@@ -0,0 +1,19 @@
+/*
+ * DPSolutionTest.cc
+ *
+ *  Author: Erwin
+ */
+
+#include "LeetCode/DPSolution.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+TEST(DPSolutionTest, climbStairs) {
+  int n = 3;
+  int res = 3;
+
+  EXPECT_EQ(DPSolution().climbStairs(n), res);
+}
+
+}  // namespace
